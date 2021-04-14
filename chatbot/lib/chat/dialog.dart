11d4005c2ba1e8bd2683ca30e12b5ea61f4802c6
Misha <misha2001du@gmail.com>
2021-04-14 23:22:15 +0300

    I've upgraded links and did scenario for bot

--- chatbot/lib/chat/dialog.dart
+++ chatbot/lib/chat/dialog.dart
@@ -1,5 +1,7 @@
 import 'package:chatbot/chat/message.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:link/link.dart';
 
 class DialogView extends StatefulWidget {
   DialogView({Key key}) : super(key: key);
@@ -64,7 +66,10 @@ class _DialogViewState extends State<DialogView> {
                         onTap: () {
                           _sendButtonData(context, buttonList[poss]);
                         },
-                        child: ButtonsCard(buttonList[poss]),
+                        child: MouseRegion(
+                          cursor: SystemMouseCursors.click,
+                          child: ButtonsCard(buttonList[poss]),
+                        ),
                       );
                     },
                   ),
@@ -93,25 +98,36 @@ class _DialogViewState extends State<DialogView> {
             ? Alignment.centerRight
             : Alignment.centerLeft,
         child: Container(
-            padding: EdgeInsets.only(right: 10, top: 5, bottom: 5, left: 10),
-            decoration: BoxDecoration(
-              color:
-                  message.from_bot == 0 ? Colors.grey[300] : Colors.grey[350],
-              borderRadius: message.from_bot == 0
-                  ? BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                      bottomLeft: Radius.circular(10))
-                  : BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                      bottomRight: Radius.circular(10)),
-            ),
-            child: Text(
-              message.text,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(color: Colors.black, fontSize: 20),
-            )),
+          padding: EdgeInsets.only(right: 10, top: 5, bottom: 5, left: 10),
+          decoration: BoxDecoration(
+            color: message.from_bot == 0 ? Colors.grey[300] : Colors.grey[350],
+            borderRadius: message.from_bot == 0
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                    bottomLeft: Radius.circular(10))
+                : BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                    bottomRight: Radius.circular(10)),
+          ),
+          child: message.link.isNotEmpty
+              ? Link(
+                  url: message.link,
+                  child: Text(
+                    message.text,
+                    style: TextStyle(color: Colors.blue[300], fontSize: 20),
+                  ),
+                )
+              : Text(
+                  message.text,
+                  style: TextStyle(
+                      color: message.text == "Например такие..."
+                          ? Colors.blue[300]
+                          : Colors.black,
+                      fontSize: 20),
+                ),
+        ),
       ),
     );
   }
@@ -153,7 +169,6 @@ class _DialogViewState extends State<DialogView> {
   }
 
   void _sendMessage(BuildContext context, Message message) {
-    print(message.id);
     setState(() {
       messageList.insert(0, message);
       this.id += 1;
@@ -171,12 +186,424 @@ class _DialogViewState extends State<DialogView> {
 
   Future<List<String>> scenario(BuildContext context, String data) async {
     switch (data) {
+      case "В начало":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context, Message(this.id, 1, "Чем могу Вам помочь?", ""));
+          var list = [
+            "Главная страница",
+            "О проекте",
+            "Обучение",
+            "Вход/Регистрация",
+            "Доп. информация"
+          ];
+          return list;
+        }
       case "Вызвать помощника":
         {
-          _sendMessage(context, Message(this.id, 0, data));
+          _sendMessage(context, Message(this.id, 0, data, ""));
           await Future.delayed(Duration(seconds: 1));
-          _sendMessage(context, Message(this.id, 1, "Чем могу Вам помочь?"));
-          var list = ["Поддержка", "Пользовательское соглашение"];
+          _sendMessage(
+              context,
+              Message(
+                  this.id,
+                  1,
+                  "Привет! Я бот Алёша! Помогатель-выручатель на сайте 'Россия - страна возможностей'. Итак...",
+                  ""));
+          await Future.delayed(Duration(seconds: 2));
+          _sendMessage(
+              context, Message(this.id, 1, "Небольшая инструкция:", ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(
+                  this.id,
+                  1,
+                  "С помощью кнопок снизу Вы будете выбирать разделы, которые Вам нужны (они листабельны, нужно лишь провести пальцем или навести на них мышью и промотать), а я буду присылать в ответ подразделы или же ссылки на нужные страницы, которые будут голубого цвета. На них нужно лишь нажать!",
+                  ""));
+          await Future.delayed(Duration(seconds: 5));
+          _sendMessage(context, Message(this.id, 1, "Например такие...", ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(context, Message(this.id, 1, "Начнём!", ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context, Message(this.id, 1, "Чем могу Вам помочь?", ""));
+          var list = [
+            "Главная страница",
+            "О проекте",
+            "Обучение",
+            "Вход/Регистрация",
+            "Доп. информация"
+          ];
+          return list;
+        }
+      case "Главная страница":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(this.id, 1, "Вот ссылка на главную страницу нашего сайта",
+                  "https://rsv.ru/"));
+          var list = [
+            "О проекте",
+            "Обучение",
+            "Вход/Регистрация",
+            "Доп. информация"
+          ];
+          return list;
+        }
+      case "О проекте":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context, Message(this.id, 1, "Информация о нашем проекте", ""));
+          var list = [
+            "В начало",
+            "О нас",
+            "Новости",
+            "Наблюдательный совет",
+            "Проекты",
+            "Истории успеха",
+            "Блог",
+            "Вся команда",
+            "Конкурсы",
+            "Галерея"
+          ];
+          return list;
+        }
+      case "О нас":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(this.id, 1, "Вот информация о нас",
+                  "https://rsv.ru/about-us/"));
+          var list = ["В начало", "О проекте"];
+          return list;
+        }
+      case "Новости":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(
+                  this.id, 1, "Все свежие новости", "https://rsv.ru/news/"));
+          var list = ["В начало", "О проекте"];
+          return list;
+        }
+      case "Наблюдательный совет":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(this.id, 1, "Наш наблюдательный совет",
+                  "https://rsv.ru/supervisory-board/"));
+          var list = ["В начало", "О проекте"];
+          return list;
+        }
+      case "Проекты":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(this.id, 1, "Все наши проекты",
+                  "https://rsv.ru/competitions/contests/"));
+          var list = ["В начало", "О проекте"];
+          return list;
+        }
+      case "Истории успеха":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(context,
+              Message(this.id, 1, "Истории успеха", "https://rsv.ru/success/"));
+          var list = ["В начало", "О проекте"];
+          return list;
+        }
+      case "Блог":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context, Message(this.id, 1, "Блог", "https://rsv.ru/blog/"));
+          var list = ["В начало", "О проекте"];
+          return list;
+        }
+      case "Вся команда":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(
+                  this.id, 1, "Вся команда", "https://rsv.ru/about-us/team/"));
+          var list = ["В начало", "О проекте"];
+          return list;
+        }
+      case "Конкурсы":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(this.id, 1, "Конкурсы",
+                  "https://rsv.ru/competitions/contests/"));
+          var list = ["В начало", "О проекте"];
+          return list;
+        }
+      case "Галерея":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(context,
+              Message(this.id, 1, "Галерея", "https://rsv.ru/gallery/"));
+          var list = ["В начало", "О проекте"];
+          return list;
+        }
+      case "Обучение":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context, Message(this.id, 1, "Информация про обучение", ""));
+          var list = [
+            "В начало",
+            "Онлайн-курсы",
+            "Очные мероприятия",
+            "Вебинары",
+            "Тесты",
+            'Программа "Наставничество"'
+          ];
+          return list;
+        }
+      case "Онлайн-курсы":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(
+                  this.id, 1, "Онлайн-курсы", "https://rsv.ru/edu/courses/"));
+          var list = ["В начало", "Обучение"];
+          return list;
+        }
+      case "Очные мероприятия":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(this.id, 1, "Очные мероприятия",
+                  "https://rsv.ru/edu/events/"));
+          var list = ["В начало", "Обучение"];
+          return list;
+        }
+      case "Вебинары":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(context,
+              Message(this.id, 1, "Вебинары", "https://rsv.ru/edu/webinars/"));
+          var list = ["В начало", "Обучение"];
+          return list;
+        }
+      case "Тесты":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context, Message(this.id, 1, "Тесты", "https://rsv.ru/testing/"));
+          var list = ["В начало", "Обучение"];
+          return list;
+        }
+      case 'Программа "Наставничество"':
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(this.id, 1, 'Программа "Наставничество"',
+                  "https://rsv.ru/mentoring/"));
+          var list = ["В начало", "Обучение"];
+          return list;
+        }
+      case "Вход/Регистрация":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context, Message(this.id, 1, "Выберите, что Вам нужно", ""));
+          var list = ["В начало", "Вход", "Регистрация"];
+          return list;
+        }
+      case "Вход":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(context,
+              Message(this.id, 1, "Вход", "https://auth.rsv.ru/login/form"));
+          var list = ["В начало", "Вход/Регистрация"];
+          return list;
+        }
+      case "Регистрация":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(this.id, 1, "Регистрация",
+                  "https://auth.rsv.ru/registration/form"));
+          var list = ["В начало", "Вход/Регистрация"];
+          return list;
+        }
+      case "Доп. информация":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(context,
+              Message(this.id, 1, "Дополнительная информация для Вас", ""));
+          var list = [
+            "В начало",
+            "Мы в соцсетях",
+            "Трудоустройство",
+            "Контакты",
+            "Политика обработки персональных данных",
+            "Пользовательское соглашение"
+          ];
+          return list;
+        }
+      case "Мы в соцсетях":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context, Message(this.id, 1, "Мы в социальных сетях!", ""));
+          var list = [
+            "В начало",
+            "Доп. информация",
+            "ВКонтакте",
+            "Одноклассники",
+            "Инстаграм",
+            "Фейсбук",
+            "Телеграм",
+            "Ютуб"
+          ];
+          return list;
+        }
+      case "ВКонтакте":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(context,
+              Message(this.id, 1, "Мы в ВКонтакте!", "https://vk.com/rsvru"));
+          var list = ["В начало", "Доп. информация"];
+          return list;
+        }
+      case "Одноклассники":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(
+                  this.id, 1, "Мы в Одноклассниках!", "https://ok.ru/rsvru"));
+          var list = ["В начало", "Доп. информация"];
+          return list;
+        }
+      case "Инстаграм":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(this.id, 1, "Мы в Инстаграм!",
+                  "https://www.instagram.com/rsv.ru/"));
+          var list = ["В начало", "Доп. информация"];
+          return list;
+        }
+      case "Фейсбук":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(this.id, 1, "Мы в Фейсбук!",
+                  "https://www.facebook.com/anorsv"));
+          var list = ["В начало", "Доп. информация"];
+          return list;
+        }
+      case "Телеграм":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(this.id, 1, "Наш Телеграм-канал!",
+                  "https://t.me/stranavozmojnostey"));
+          var list = ["В начало", "Доп. информация"];
+          return list;
+        }
+      case "Ютуб":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(this.id, 1, "Наш Ютуб-канал!",
+                  "https://www.youtube.com/channel/UCJPc1Fjh9C8tPUo5qBEErMw"));
+          var list = ["В начало", "Доп. информация"];
+          return list;
+        }
+      case "Трудоустройство":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(
+                  this.id, 1, "Найдите работу с нами", "https://trudvsem.ru/"));
+          var list = ["В начало", "Доп. информация"];
+          return list;
+        }
+      case "Контакты":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(this.id, 1, "Наши контактные данные!",
+                  "https://rsv.ru/contacts/"));
+          var list = ["В начало", "Доп. информация"];
+          return list;
+        }
+      case "Политика обработки персональных данных":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(this.id, 1, "Политика обработки персональных данных",
+                  "https://s3-cms.rsv.ru/a4ab0c5af82ae400423caf415c51f33362ee561240e3a53d9c465307fb81a7ea.pdf"));
+          var list = ["В начало", "Доп. информация"];
+          return list;
+        }
+      case "Пользовательское соглашение":
+        {
+          _sendMessage(context, Message(this.id, 0, data, ""));
+          await Future.delayed(Duration(seconds: 1));
+          _sendMessage(
+              context,
+              Message(this.id, 1, "Пользовательское соглашение",
+                  "https://s3-cms.rsv.ru/720f72cf824a72142e6bab5b41fb1f6b9d2ca8cbc39551381b33e762ebe90ad7.doc"));
+          var list = ["В начало", "Доп. информация"];
           return list;
         }
     }
--- chatbot/lib/chat/message.dart
+++ chatbot/lib/chat/message.dart
@@ -2,12 +2,14 @@ class Message {
   int _id;
   int _from_bot;
   String _text;
+  String _link;
 
-  Message(this._id, this._from_bot, this._text);
+  Message(this._id, this._from_bot, this._text, this._link);
 
   int get id => _id;
   int get from_bot => _from_bot;
   String get text => _text;
+  String get link => _link;
 
   set setId(int new_id) {
     this._id = new_id;
@@ -19,5 +21,9 @@ class Message {
 
   set setText(String new_text) {
     this._text = new_text;
+  }
+
+  set setLink(String new_link) {
+    this._link = new_link;
   }
 }
